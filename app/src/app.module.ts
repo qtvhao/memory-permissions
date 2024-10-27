cf1036067ccad9ed34b4327987db4442b1543abc
Your Name <you@example.com>
2024-10-27 15:11:55 +0100

    Add PermissionsModule to app.module.ts

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AccessKeysModule } from './access-keys/access-keys.module';
+import { PermissionsModule } from './permissions/permissions.module';
 
 @Module({
-  imports: [UsersModule, AccessKeysModule],
+  imports: [UsersModule, AccessKeysModule, PermissionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
