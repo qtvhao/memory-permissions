b097d6074e7804a719b0317f318f42409f1431ee
Your Name <you@example.com>
2024-10-28 09:59:12 +0100

    Refactor import path for UsersModule in AccessKeysModule

--- app/src/access-keys/access-keys.module.ts
+++ app/src/access-keys/access-keys.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AccessKeysService } from './access-keys.service';
 import { AccessKeysController } from './access-keys.controller';
 import { PermissionsModule } from '../permissions/permissions.module';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [PermissionsModule, UsersModule],
